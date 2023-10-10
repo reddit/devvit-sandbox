ad9eb0b7c0e3759f7ab1e7796a8243d1cbb509d8
pl00h <139405650+pl00h@users.noreply.github.com>
2023-10-09 21:05:26 -0400

    Icon gallery (#16)
    <!-- If this pull request closes an issue, please mention the issue
    number below -->
    Closes # <!-- Issue # here -->
    
    ## 💸 TL;DR
    <!-- What's the three sentence summary of purpose of the PR -->
    
    ## 📜 Details
    [Design Doc](<!-- insert Google Doc link here if applicable -->)
    
    [Jira](<!-- insert Jira link if applicable -->)
    
    <!-- Add additional details required for the PR: breaking changes,
    screenshots, external dependency changes -->
    
    ## 🧪 Testing Steps / Validation
    <!-- add details on how this PR has been tested, include reproductions
    and screenshots where applicable -->
    
    ## ✅ Checks
    <!-- Make sure your pr passes the CI checks and do check the following
    fields as needed - -->
    - [ ] CI tests (if present) are passing
    - [ ] Adheres to code style for repo
    - [ ] Contributor License Agreement (CLA) completed if not a Reddit
    employee

---
+++ icons-gallery/src/components/Columns.tsx
@@ -0,0 +1,26 @@
+import { Devvit } from '@devvit/public-api';
+
+type ColumnProps = {
+    count: number
+    children: JSX.Children
+  }
+
+export const Columns = ({ count, children }: ColumnsProps): JSX.Element => {
+  const rows: JSX.Element[] = [];
+
+  // Divide the children into rows with N (count) children each.
+  // Pad the last row if incomplete.
+  const width = 100 / count;
+  for (let i = 0; i < children.length; i += count) {
+    const row = children.slice(i, i + count).map((c) => (
+      <vstack width={width}>
+        <hstack grow>{c}</hstack>
+      </vstack>
+    ));
+    const rowHasRoom = (): boolean => row.length < count;
+    while (rowHasRoom()) row.push(<hstack width={width} />);
+    rows.push(<hstack gap="small">{row}</hstack>);
+  }
+
+  return <vstack gap="small">{rows}</vstack>;
+};
